175254bb24c355a0857486c66efb55b2db6de5ef
Cassio Cabral <cassioscabral@gmail.com>
2013-12-14 22:17:37 -0300

    testando com latency compensation

--- client/views/posts/post_submit.js
+++ client/views/posts/post_submit.js
@@ -12,7 +12,8 @@ Template.postSubmit.events({
       if (error)
         return alert(error.reason);
 
-      Router.go('postPage', {_id: id});
+      //Router.go('postPage', {_id: id});
+      Router.go('postsList');
     });
   }
 });
--- collections/posts.js
+++ collections/posts.js
@@ -20,14 +20,23 @@ Meteor.methods({
     }
 
     // pick out the whitelisted keys
-    var post = _.extend(_.pick(postAttributes, 'url', 'title', 'message'), {
+    var post = _.extend(_.pick(postAttributes, 'url', 'message'), {
+      title: postAttributes.title + (this.isSimulation ? '(client)' : '(server)'),
       userId: user._id, 
       author: user.username, 
       submitted: new Date().getTime()
     });
-
+    // wait for 5 seconds
+    if (! this.isSimulation) {
+      var Future = Npm.require('fibers/future');
+      var future = new Future();
+      Meteor.setTimeout(function() {
+        future.return();
+      }, 5 * 1000);
+      future.wait();
+    }
     var postId = Posts.insert(post);
-
     return postId;
+  }
   }
 });
