f47589a8b93bd4ad7f7453ecd0b962b714d30801
Cassio Cabral <cassioscabral@gmail.com>
2013-12-16 18:36:55 -0300

    added limit posts in the route

--- server/publications.js
+++ server/publications.js
@@ -1,5 +1,5 @@
-Meteor.publish('posts', function() {
-  return Posts.find();
+Meteor.publish('posts', function(options) {
+  return Posts.find({}, options);
 });
 Meteor.publish('comments', function(postId) {
   return Comments.find({postId: postId});
