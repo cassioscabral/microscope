2262989895c09f38e8eb364321a0a11f57df73f1
Cassio Cabral <cassioscabral@gmail.com>
2013-12-16 17:15:48 -0300

    notification collection

--- collections/comments.js
+++ collections/comments.js
@@ -10,12 +10,16 @@ Meteor.methods({
       throw new Meteor.Error(422, 'Please write some content');
     if (!post)
       throw new Meteor.Error(422, 'You must comment on a post');
-    comment = _.extend(_.pick(commentAttributes, 'postId', 'body'), {
+      comment = _.extend(_.pick(commentAttributes, 'postId', 'body'), {
       userId: user._id,
       author: user.username,
       submitted: new Date().getTime()
     });
     Posts.update(comment.postId, {$inc: {commentsCount: 1}});
-    return Comments.insert(comment);
+    // create the comment, save the id
+    comment._id = Comments.insert(comment);
+    // now create a notification, informing the user that there's been a comment
+    createCommentNotification(comment);
+    return comment._id;
   }
 });
--- server/publications.js
+++ server/publications.js
@@ -4,3 +4,6 @@ Meteor.publish('posts', function() {
 Meteor.publish('comments', function(postId) {
   return Comments.find({postId: postId});
 });
+Meteor.publish('notifications', function() {
+  return Notifications.find();
+});
