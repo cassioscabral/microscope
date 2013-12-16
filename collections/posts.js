01c322ab17a21ad258153ac64bc1e79d38c673ec
Cassio Cabral <cassioscabral@gmail.com>
2013-12-16 11:06:14 -0300

    error report

--- collections/posts.js
+++ collections/posts.js
@@ -3,6 +3,12 @@ Posts.allow({
   update: ownsDocument,
   remove: ownsDocument
 });
+Posts.deny({
+  update: function(userId, post, fieldNames) {
+    // may only edit the following two fields:
+    return (_.without(fieldNames, 'url', 'title').length > 0);
+  }
+});
 Meteor.methods({
   post: function(postAttributes) {
     var user = Meteor.user(),
